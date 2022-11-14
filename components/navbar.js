12504a8fcbc201a9135476dbe8ae1d94218e00b4
kill3rBi4d <killrbid@gmail.com>
2022-11-14 14:10:56 +0300

    some changes

--- components/navbar.js
+++ components/navbar.js
@@ -2,13 +2,13 @@ const navbar = {
     render: async () => {
         let view = `
             <div class="nav-wrapper">
-                <a class="nav-item" href="san-router-demo/#/">
+                <a class="nav-item home-router">
                     <span>Home</span>
                 </a>
-                <a class="nav-item" href="san-router-demo/#/about">
+                <a class="nav-item about-router">
                     <span>About</span>
                 </a>
-                <a class="nav-item" href="san-router-demo/#/projects">
+                <a class="nav-item projects-router">
                     <span>Projects</span>
                 </a>
             </div>
@@ -16,6 +16,15 @@ const navbar = {
         return view;
     },
     after_render: async () => {
+        document.querySelector('.home-router').addEventListener('click', () =>{
+            location.hash = '/';
+        })
+        document.querySelector('.about-router').addEventListener('click', () =>{
+            location.hash = '/about';
+        })
+        document.querySelector('.projects-router').addEventListener('click', () =>{
+            location.hash = '/projects';
+        })
         // let navbar = document.getElementById('navbar');
     }
 }
