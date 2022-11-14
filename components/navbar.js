7f10a551481a1e117be06a4146de07cf707994fb
kill3rBi4d <killrbid@gmail.com>
2022-11-14 14:03:38 +0300

    new publish

--- components/navbar.js
+++ components/navbar.js
@@ -2,13 +2,13 @@ const navbar = {
     render: async () => {
         let view = `
             <div class="nav-wrapper">
-                <a class="nav-item" href="/#/">
+                <a class="nav-item" href="san-router-demo/#/">
                     <span>Home</span>
                 </a>
-                <a class="nav-item" href="/#/about">
+                <a class="nav-item" href="san-router-demo/#/about">
                     <span>About</span>
                 </a>
-                <a class="nav-item" href="/#/projects">
+                <a class="nav-item" href="san-router-demo/#/projects">
                     <span>Projects</span>
                 </a>
             </div>
